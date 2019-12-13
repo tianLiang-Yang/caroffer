8fde7ba360bbe1f72e183992147d277dc531ab48
小小嘿客 <50702784+YaoYao-Ling@users.noreply.github.com>
2019-12-13 21:10:36 +0800

    12.13

--- src/services/modules/pic.js
+++ src/services/modules/pic.js
@@ -1,18 +1,11 @@
-/*
- * @Author: 席鹏昊
- * @Date: 2019-12-03 13:41:05
- * @LastEditors: 席鹏昊
- * @LastEditTime: 2019-12-05 18:55:07
- * @Description: 
- */
-import Axios from "@/utils/index.js";
+import Axios from "@/utils/request.js";
 
 // 获取图片列表
-export let getImageList=(params)=>{
-    return Axios("/v2-car-getImageList.html", {params})
+export let getImageList = (params) => {
+    return Axios("/v2-car-getImageList.html", { params })
 }
 
 // 获取图片分类列表
-export let getImageTypeList=(params)=>{
-    return Axios("/v2-car-getCategoryImageList.html", {params})
+export let getImageTypeList = (params) => {
+    return Axios("/v2-car-getCategoryImageList.html", { params })
 }
--- vue.config.js
+++ vue.config.js
@@ -20,4 +20,6 @@ module.exports = {
             }
         }
     },
+    publicPath: '/1704B/yangtianliang/',
+    productionSourceMap: false
 }
